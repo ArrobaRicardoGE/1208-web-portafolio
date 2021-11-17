e0ed16394245133d7fff762e588451bf5a3a5d86
ArrobaRicardoGE <ricardoarroba@outlook.com>
2021-11-16 21:21:49 -0600

    [Practica 20] Primera version

---
+++ Practicas/practica21/script.js
@@ -0,0 +1,62 @@
+function loadData() {
+    localStorage.users = JSON.stringify({ admin: '123', user: '123' });
+    localStorage.products = JSON.stringify([{
+            name: 'Forza Horizons 5',
+            price: 3199
+        },
+        {
+            name: 'Streer Fighter',
+            price: 129
+        },
+        {
+            name: 'Rocket League',
+            price: 'Free'
+        },
+        {
+            name: 'Red Dead Redemption 2',
+            price: 1099
+        },
+        {
+            name: 'Cyber Punk 2077',
+            price: 1099
+        },
+        {
+            name: 'FIFA 2021',
+            price: 899
+        },
+        {
+            name: 'Star Wars: Battlefront',
+            price: 599
+        },
+        {
+            name: 'Grand Theft Auto 5',
+            price: 799
+        },
+        {
+            name: 'Madden 22',
+            price: 1399
+        },
+        {
+            name: 'Genshi Impact',
+            price: 189
+        }
+    ]);
+}
+
+function logIn() {
+    user = document.getElementById('user').value;
+    password = document.getElementById('password').value;
+    users = JSON.parse(localStorage.users);
+    if (!users.hasOwnProperty(user)) {
+        alert('Usuario inválido');
+        return;
+    }
+    if (users[user] != password) {
+        alert('Contraseña incorrecta');
+        return;
+    }
+    sessionStorage.user = user;
+    window.location.replace('dashboard.html');
+}
+
+loadData();
