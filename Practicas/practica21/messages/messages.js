592cbf8093214abd9902b36c7a1ae718003722ff
ArrobaRicardoGE <ricardoarroba@outlook.com>
2021-11-20 08:29:33 -0600

    [Practica 21] Crear sistema de mensajeria

---
+++ Practicas/practica21/messages/messages.js
@@ -0,0 +1,32 @@
+function validate() {
+    if ("admin" != sessionStorage.user) {
+        alert("Credenciales inválidas. Favor de identificarse.");
+        document.body.innerHTML = "";
+    }
+}
+
+function populate() {
+    if (!localStorage.messages) return;
+    let messages = JSON.parse(localStorage.messages);
+    let dummmy = document.createElement("div");
+    for (let i = 0; i < messages.length; i++) {
+        dummmy.innerHTML = `<li
+                                    class="
+                                        list-group-item
+                                        d-flex
+                                        justify-content-between
+                                        align-items-start
+                                    "
+                                >
+                                    <div class="ms-2 me-auto">
+                                        <div class="fw-bold">De: ${messages[i].email}</div>
+                                        Mensaje: ${messages[i].message}
+                                    </div>
+                                </li>`;
+        document
+            .getElementById("message-list")
+            .appendChild(dummmy.firstElementChild);
+    }
+}
+
+populate();
